fe9ac0c7138078626903c1b23e81bd6137153950
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-24 02:01:21 +0200

    custom email api

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ const adminApp = admin.initializeApp({
 })
 
 const corsOption = {
-  origin: 'https://firebase-auth-and-crud.vercel.app/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers'],
-  preflightContinue: false,
+  origin: true,
   optionsSuccessStatus: 200
 }
 
