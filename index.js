6c5fc959ce7744cd639624aeec9eae9c796b4628
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-24 16:07:55 +0200

    custom email api

--- index.js
+++ index.js
@@ -63,9 +63,7 @@ app.post('/send-custom-verification-email', async (req, res) => {
 
     })
     .catch((err) => {
-      console.log("Could not verify user: ", err.userInfo)
       const message = error.message
-      console.log(message);
       if (error.code === 'auth/user-not-found') {
         return res.status(404).json({ message })
       }
