e2320f8e48ab289617bb0f27dbac7d3a3a1e2f43
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-24 23:57:41 +0200

    custom email api

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ const sendVerificationEmail = require('./sendEmail')
 const adminApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 })
+const auth = adminApp.auth();
 
 const corsOption = {
   origin: '*',
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization, Content-Length, X-Requested-With'
 }
 
 const PORT = process.env.PORT || 8000
@@ -24,7 +27,15 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 app.use(Cors(corsOption));
-app.use(express.json())
+app.use(express.json());
+
+//set up cors using this method, prefered over cors package
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 // routes
 app.get('/', (req, res) => {
@@ -48,32 +59,33 @@ app.post('/send-custom-verification-email', async (req, res) => {
 
   const actionLink = await getAuth().generateEmailVerificationLink(userEmail, actionCodeSettings)
 
-  applyActionCode(auth, actionCode)
-    .then(async (resp) => {
-      const { email } = resp.user
-      const ejspath = path.join(process.cwd(), 'views/verify-email.ejs')
-      const template = await ejs.renderFile(
-        ejspath,
-        {
-          actionLink,
-          randomNumber: Math.random()
-        })
-      await sendVerificationEmail(userEmail, template, actionLink)
-      res.status(200).json({ message: 'Email successfully sent' })
+  //put it in redirectUrl handler in next
+  // applyActionCode(auth, actionCode)
+  // .then(async (resp) => {
+  // const { email } = resp.user
+  const ejspath = path.join(process.cwd(), 'views/verify-email.ejs')
+  const template = await ejs.renderFile(
+    ejspath,
+    {
+      actionLink,
+      randomNumber: Math.random()
+    });
+  await sendVerificationEmail(userEmail, template, actionLink)
+  res.status(200).json({ message: 'Email successfully sent' })
 
-    })
-    .catch(error => {
-      // const message = error.message
-      if (error.code === 'auth/user-not-found') {
-        return res.status(404).json({ error });
-      }
-      if (error.code === 'auth/invalid-continue-uri') {
-        return res.status(401).json({ error });
-      }
-      res.json({ error })
-    })
-}
-);
+});
+// .catch(error => {
+//   const message = error.message
+//   if (error.code === 'auth/user-not-found') {
+//     return res.status(404).json({ message });
+//   }
+//   if (error.code === 'auth/invalid-continue-uri') {
+//     return res.status(401).json({ message });
+//   }
+//   res.status(500).json({ message })
+// })
+// }
+// );
 
 
 // listener
